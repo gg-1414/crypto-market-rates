1aadc54593715cee93d940fea9e2bf2b6173b288
Gina Lee <gina@thesistesting.com>
2020-10-27 13:44:59 -0400

    add dark theme functionality & style updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,24 +2,44 @@
   <header>
     <img alt="Vue logo" src="https://liquality.io/images/LiqualityHorizontal.svg">
     <h1>Crypto Market Rates</h1>
-    <div class="interval-toggle">
+    <div class="toggle-interval">
       <p>Refresh (seconds)</p>
-      <button @click="handleClick" class="active">5</button>
-      <button @click="handleClick">10</button>
-      <button @click="handleClick">15</button>
+      <button @click="handleIntervalClick" class="active">5</button>
+      <button @click="handleIntervalClick">10</button>
+      <button @click="handleIntervalClick">15</button>
     </div>
+    <button class="toggle-theme" @click="handleToggleClick">
+      <sun-icon v-if="!!darkTheme"/>
+      <moon-icon v-else/>
+    </button>
   </header>
 </template>
 
 <script>
+import MoonIcon from './icons/Moon.vue'
+import SunIcon from './icons/Sun.vue'
+
 export default {
   name: 'Header',
+  components: {
+    MoonIcon,
+    SunIcon
+  },
+  props: {
+    darkTheme: {
+      type: Boolean,
+      default: false
+    }
+  },
   methods: {
-    handleClick(event) {
-      const activeButton = document.querySelector('.interval-toggle button.active')
+    handleIntervalClick(event) {
+      const activeButton = document.querySelector('.toggle-interval button.active')
       activeButton.classList.remove('active')
       event.target.classList.add('active')
       this.$emit('interval-change', event.target.innerText)
+    },
+    handleToggleClick() {
+      this.$emit('theme-change', !this.darkTheme)
     }
   }
 }
@@ -28,15 +48,47 @@ export default {
 <style lang="scss" scoped>
   @import '../styles/theme.scss';
 
+  $toggle-theme-btn-width: 36px;
+
+  body {
+    &.theme-dark {
+      .toggle-interval button {
+        border: 2px solid $white;
+      }
+    }
+  }
+
+  button {
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+    &:focus { outline: none; }
+  }
+
   header {
     display: grid;
+    grid-template-columns: 1fr calc($toggle-theme-btn-width + 16px);
+    grid-template-areas: 
+      "logo themeToggle"
+      "title title"
+      "intervalToggle intervalToggle";
 
     h1 {
-      font-size: 24px;
+      font-size: 28px;
+      grid-area: title;
     }
 
     img {
-      justify-self: center;
+      justify-self: start;
+      grid-area: logo;
+    }
+
+    .toggle-interval {
+      grid-area: intervalToggle;
+    }
+
+    .toggle-theme {
+      grid-area: themeToggle;
+      justify-self: end;
     }
 
     @media screen and (min-width: 768px) {
@@ -51,26 +103,38 @@ export default {
         grid-area: 2 / 1;
       }
 
-      .interval-toggle {
+      .toggle-interval {
         justify-self: right;
       }
 
-      .interval-toggle {
+      .toggle-interval {
         grid-area: 2 / 2;
       }
     }
 
     @media screen and (min-width: 1280px) {
-      display: flex;
-      justify-content: space-between;
+      display: grid;
+      grid-template-columns: auto auto auto calc(#{$toggle-theme-btn-width} + 24px);
+      grid-template-rows: 1fr;
+      align-items: center;
       
       h1 {
         font-size: 32px;
+        grid-area: 1 / 2;
+        justify-self: center;
+      }
+
+      .toggle-interval {
+        grid-area: 1 / 3;
+      }
+
+      .toggle-theme {
+        grid-area: 1 / 4;
       }
     }
   }
 
-  .interval-toggle {
+  .toggle-interval {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -88,21 +152,31 @@ export default {
       font-weight: bold;
       letter-spacing: 1px;
       background: $light-blue-gray;
-      border: 2px solid $black;
-      cursor: pointer;
+      border: 2px solid $darkest-blue-gray;
 
       &:hover,
       &.active {
         background: linear-gradient(45deg, $teal, $magenta);
       }
-
-      &:focus {
-        outline: none;
-      }
     }
 
     @media screen and (min-width: 768px) {
       justify-content: right;
+    }
+  }
+
+  .toggle-theme {
+    border: none;
+    height: 32px;
+    width: $toggle-theme-btn-width;
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+    align-self: center;
+    background-color: $light-gray;
+
+    svg {
+      width: 100%;
     }
   }
 </style>
--- src/components/Row.vue
+++ src/components/Row.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row">
-    <div class="type"> 
+    <p class="type"> 
       <b>{{ content.from }}</b> to <b>{{ content.to }}</b>
-    </div>
+    </p>
     <span class="rate">{{ content.rate }}</span>
   </div>
 </template>
@@ -22,17 +22,27 @@ export default {
 <style lang="scss" scoped>
   @import '../styles/theme.scss';
 
+  body {
+    &.theme-dark {
+      .row {
+        background-color: $light-blue-gray;
+      }
+    }
+  }
+
   .row {
     display: flex;
     align-items: center;
     text-align: left;
     gap: 16px;
-    background-color: $light-gray;
     padding: 16px;
+    border-radius: 5px;
+    background-color: $light-gray;
   }
 
   .type {
     flex: 1.3;
+    margin: 0;
   }
 
   .rate {
